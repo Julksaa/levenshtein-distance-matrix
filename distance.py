c7859275db026d8767b788c5bad1fb6fdc3d7249
Julksaa <vellyzxg5@gmail.com>
2026-08-23 16:26:38 +0700

    feat: add distance.py

---
+++ distance.py
@@ -0,0 +1,10 @@
+def levenshtein(s1: str, s2: str) -> int:
+    if len(s1) < len(s2): return levenshtein(s2, s1)
+    if len(s2) == 0: return len(s1)
+    prev = range(len(s2) + 1)
+    for i, c1 in enumerate(s1):
+        curr = [i + 1]
+        for j, c2 in enumerate(s2):
+            curr.append(min(prev[j + 1] + 1, curr[j] + 1, prev[j] + (c1 != c2)))
+        prev = curr
+    return prev[-1]
